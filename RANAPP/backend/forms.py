79184c9318d92a9ddd68ef754a675a15b719359d
Your Name <you@example.com>
2022-05-05 11:40:12 +0545

    RAN APP UI

--- RANAPP/backend/forms.py
+++
@@ -1,6 +0,0 @@
-# from django import forms
-# from .models import membership
-# class membershipform(forms.ModelForm):
-#     class Meta:
-#         model = membership
-#         fields = ['name', 'membership type' , 'year' , 'socialsites1' , 'socialsites2' , 'socialsites3' ]
